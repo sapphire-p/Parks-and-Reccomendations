6225c08ac3df74cee4cc70321d6d5a74c6af60b5
Fran <36076468+francescasmith@users.noreply.github.com>
2021-07-07 19:55:43 +0100

    Splice Results

--- js/app.js
+++ js/app.js
@@ -24,31 +24,31 @@ let arrayOfParks = []; // This creates an empty array which will contain all of 
 // Each of the below creates a new park object (using the ParkObject class template above)
 // The new park object is then pushed into the arrayOfParks array
 
-let greenwichPark = new ParkObject("Greenwich Park", "South", "Medium", true, true, true, true, "North Greenwich", "Greenwich Park is one of the Royal Parks of London and is situated in the south-east of the city. Its hills offer excellent views over the River Thames, the Isle of Dogs and the City of London.");
+let greenwichPark = new ParkObject("Greenwich Park", "South", "Medium", true, true, true, true, "North Greenwich", "Greenwich Park is one of the Royal Parks of London and is situated in the south-east of the city. Its hills offer excellent views over the River Thames, the Isle of Dogs and the City of London. ");
 arrayOfParks.push(greenwichPark);
 
-let claphamCommon = new ParkObject("Clapham Common", "West", "Large", true, true, false, true, "Clapham Common", "Clapham Common is a large triangular urban park in Clapham, south London, England. It is 220 acres of green space, with three ponds and a Victorian bandstand.")
+let claphamCommon = new ParkObject("Clapham Common", "West", "Large", true, true, false, true, "Clapham Common", "Clapham Common is a large triangular urban park in Clapham, south London, England. It is 220 acres of green space, with three ponds and a Victorian bandstand. ")
 arrayOfParks.push(claphamCommon);
 
-let batterseaPark = new ParkObject("Battersea Park", "West", "Medium", true, true, true, true, "Queenstown Road (Battersea)", "Battersea Park is a 200-acre green space at Battersea in the London Borough of Wandsworth. It is situated on the south bank of the River Thames opposite Chelsea and occupies marshland reclaimed from the Thames and land formerly used for market gardens.");
+let batterseaPark = new ParkObject("Battersea Park", "West", "Medium", true, true, true, true, "Queenstown Road (Battersea)", "Battersea Park is a 200-acre green space at Battersea in the London Borough of Wandsworth. It is situated on the south bank of the River Thames opposite Chelsea and occupies marshland reclaimed from the Thames and land formerly used for market gardens. ");
 arrayOfParks.push(batterseaPark);
 
-let hydePark = new ParkObject("Hyde Park", "Central", "Large", true, true, true, true, "Hyde Park Corner", "Hyde Park is a Grade I-listed major park in central London and the largest of the Royal Parks in central London. The park is divided by the Serpentine and the Long Water lakes.");
+let hydePark = new ParkObject("Hyde Park", "Central", "Large", true, true, true, true, "Hyde Park Corner", "Hyde Park is a Grade I-listed major park in central London and the largest of the Royal Parks in central London. The park is divided by the Serpentine and the Long Water lakes. ");
 arrayOfParks.push(hydePark);
 
-let nunheadCemetery = new ParkObject("Nunhead Cemetery", "South", "Small", false, false, true, false, "Nunhead", "Nunhead Cemetery is one of the 'Magnificent Seven' large private cemeteries in London. It was originally known as All Saints' Cemetery, consecrated in 1840 and is a local nature reserve.");
+let nunheadCemetery = new ParkObject("Nunhead Cemetery", "South", "Small", false, false, true, false, "Nunhead", "Nunhead Cemetery is one of the 'Magnificent Seven' large private cemeteries in London. It was originally known as All Saints' Cemetery, consecrated in 1840 and is a local nature reserve. ");
 arrayOfParks.push(nunheadCemetery);
 
-let crystalPalacePark = new ParkObject("Crystal Palace Park", "South", "Medium", true, true, true, true, "Crystal Palace", "Former site of the Crystal Palace which was built as part of the 1851 Great Exhibition. Today the park consists of statues of prehistoric monsters, geological strata, open parkland and pathways, sport and recreation facilities, an animal farm, playground, maze and cafe.");
+let crystalPalacePark = new ParkObject("Crystal Palace Park", "South", "Medium", true, true, true, true, "Crystal Palace", "Former site of the Crystal Palace which was built as part of the 1851 Great Exhibition. Today the park consists of statues of prehistoric monsters, geological strata, open parkland and pathways, sport and recreation facilities, an animal farm, playground, maze and cafe. ");
 arrayOfParks.push(crystalPalacePark);
 
-let barnsburyWood = new ParkObject("Barnsbury Wood", "Central", "Small", false, false, false, false, "Highbury & Islington", "Barnsbury Wood is London's smallest local nature reserve. The hidden woodland was eventually abandoned to nature and is now home to a wealth of wildlife.");
+let barnsburyWood = new ParkObject("Barnsbury Wood", "Central", "Small", false, false, false, false, "Highbury & Islington", "Barnsbury Wood is London's smallest local nature reserve. The hidden woodland was eventually abandoned to nature and is now home to a wealth of wildlife. ");
 arrayOfParks.push(barnsburyWood);
 
-let victoriaPark = new ParkObject("Victoria Park", "East", "Large", true, true, true, true, "Hackney Wick", "Nicknamed Vicky Park or the People's Park, Victoria Park is a park in the London Borough of Tower Hamlets in East London, England. It is the largest park in Tower Hamlets and one of the most visited green spaces in London – approximately 9 million people visit each year.");
+let victoriaPark = new ParkObject("Victoria Park", "East", "Large", true, true, true, true, "Hackney Wick", "Nicknamed Vicky Park or the People's Park, Victoria Park is a park in the London Borough of Tower Hamlets in East London, England. It is the largest park in Tower Hamlets and one of the most visited green spaces in London – approximately 9 million people visit each year. ");
 arrayOfParks.push(victoriaPark);
 
-let alexandraPark = new ParkObject("Alexandra Park", "North", "Medium", false, false, false, true, " Alexandra Palace", "Alexandra Park is situated in the Borough of Haringey in north London, adjacent to Hornsey, Muswell Hill and Wood Green. It has a mixture of hilly and flat terrain. Along with the neighbouring Alexandra Palace, the park was named in 1863, the year of the marriage of Alexandra of Denmark to the Prince of Wales who became King Edward VII.");
+let alexandraPark = new ParkObject("Alexandra Park", "North", "Medium", false, false, false, true, " Alexandra Palace", "Alexandra Park is situated in the Borough of Haringey in north London, adjacent to Hornsey, Muswell Hill and Wood Green. It has a mixture of hilly and flat terrain. Along with the neighbouring Alexandra Palace, the park was named in 1863, the year of the marriage of Alexandra of Denmark to the Prince of Wales who became King Edward VII. ");
 arrayOfParks.push(alexandraPark);
 
 
@@ -111,11 +111,11 @@ function submitForm() {
                     && arrayOfParks[i].parking === parking
                     && arrayOfParks[i].cyclistFriendly === cyclistFriendly
                 ) {
-                    recommendation.push(`${arrayOfParks[i].name} (nearest station: ${arrayOfParks[i].nearestStation}) \n${arrayOfParks[i].description}`);
+                    recommendation.push("!", arrayOfParks[i].name,"!", arrayOfParks[i].nearestStation,"!", arrayOfParks[i].description)
                 }
             }
             if (recommendation.length >= 1) {
-                return recommendation.join("\n \n");
+                return recommendation;
             } else {
                 return "Sorry, but we are unable to suggest a park that matches those specific criteria. Please try again.";
             }
@@ -126,13 +126,31 @@ function submitForm() {
     resultsPage = "./results.html";
     window.open(resultsPage);  
 
-   return results
+   return false
 };
 
 
 function retrieveData() {
-   document.getElementById("results-text").innerHTML = localStorage.getItem("results")
+    let resultsForSplice = localStorage.getItem("results")
 
+    let splicedResults = resultsForSplice.split("!,")
+
+    console.log(splicedResults)
+
+    if (splicedResults.length == 1){
+        document.getElementById("results-text").innerHTML = splicedResults 
+    } else if (splicedResults.length == 4){
+        document.getElementById("results-title").innerHTML = splicedResults[1].slice(0,-1)
+        document.getElementById("nearest-station").innerHTML = splicedResults[2].slice(0,-1)
+        document.getElementById("results-description").innerHTML = splicedResults[3].slice(0,-1)
+    } else {
+        document.getElementById("results-title").innerHTML = splicedResults[1].slice(0,-1)
+        document.getElementById("nearest-station").innerHTML = splicedResults[2].slice(0,-1)
+        document.getElementById("results-description").innerHTML = splicedResults[3].slice(0,-1)
+        document.getElementById("results-title-2").innerHTML = splicedResults[4].slice(0,-1)
+        document.getElementById("nearest-station-2").innerHTML = splicedResults[5].slice(0,-1)
+        document.getElementById("results-description-2").innerHTML = splicedResults[6].slice(0,-1)
+    }
 
 }
 
